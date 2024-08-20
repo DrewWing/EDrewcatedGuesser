4ad6eb3b736d1ae9c495cf2555ca3fca2c4964a7
Drew Wingfield <89153952+DrewWing@users.noreply.github.com>
2024-08-20 11:57:27 -0500

    Add __init__.py for generating required directories

---
+++ __init__.py
@@ -0,0 +1,33 @@
+#
+# -*- coding: utf-8 -*-
+# __init__.py
+# Started 2024/08/20
+# by Drew Wingfield
+
+# This file is a part of Drew's FTCAPI project, and
+# all code is under the license found at LICENSE.txt unless otherwise
+# specified. See the README.md for more details.
+
+
+import os
+
+def make_required_directories():
+    """
+    Make directories required for the program to run if they don't already exist
+    """
+    # Make required directories if they don't exist already
+    for dir in [
+        "app/generatedfiles",
+        "app/generatedfiles/joblibcache",
+        #"app/generatedfiles/all-events",
+        #"app/generatedfiles/all-teams",
+        #"app/generatedfiles/eventdata"
+    ]:
+        if not os.path.exists(dir):
+            os.makedirs(dir)
+
+
+
+make_required_directories()
+
+# -- End of file --
