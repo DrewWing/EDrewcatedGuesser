84b5027f76c9a265dd383b97cf72b8232129fee8
Drew Wingfield <89153952+DrewWing@users.noreply.github.com>
2024-08-21 08:21:14 -0500

    Add markdown file test, cleanup test functions

--- tests/correctfiles.py
+++ tests/correctfiles.py
@@ -8,27 +8,36 @@ class correctFileTest(unittest.TestCase):
     def setUp(self):
         pass
 
+    def no_extensions_in_directory(self, extensions, dirpath, msg="A file isn't where it should be."):
+        list_of_files = os.listdir(dirpath)
+
+        for extension in extensions:
+            for file in list_of_files:
+                self.assertFalse(extension in file, msg=msg+f" - Extension  {extension} with path {file}.")
+
+
     def test_app_files(self):
         """
         Makes sure no data (generated) files are in the apps dir outside the generatedfiles dir
         """
         data_extensions = [".json",".csv",".config",".log",".txt",".npy"]
-        list_of_app_files = os.listdir("app")
-
-        for extension in data_extensions:
-            for file in list_of_app_files:
-                self.assertFalse(extension in file, msg=f"A data file of extension {extension} is in the apps folder, with path {file}. It should be in generatedfiles")
+        self.no_extensions_in_directory(extensions=data_extensions, dirpath="app",msg="Data (generated) file is in the apps dir! Should be in generatedfilesdir.")
     
     def test_generatedfiles_files(self):
         """
         Makes sure no script files are in the generatedfiles dir in the apps dir
         """
-        data_extensions = [".ps1",".sh",".py",".java",".cpp"]
-        list_of_app_files = os.listdir("app/generatedfiles")
+        script_extensions = [".ps1",".sh",".py",".java",".cpp"]
+        self.no_extensions_in_directory(script_extensions,"app/generatedfiles","A script is in the generatedfiles dir! It should be in apps!")
+    
+    def test_markdown_files(self):
+        """
+        Makes sure no markdown files are anywhere but the root directory
+        """
+        data_extensions = [".md"]
+        list_of_app_files = os.listdir("app")
 
-        for extension in data_extensions:
-            for file in list_of_app_files:
-                self.assertFalse(extension in file, msg=f"A data file of extension {extension} is in the generatedfiles folder, with path {file}. It is a script and should be in app.")
+
 
     
 if __name__ == '__main__':
