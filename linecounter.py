eaa697fd57749b5dee4fc3587a8dd2e1c3a31304
Drew Wingfield <89153952+DrewWing@users.noreply.github.com>
2024-08-28 10:19:19 -0500

    Fix Line Counter bugs, add indented counter, clean up print statements

--- linecounter.py
+++ linecounter.py
@@ -12,9 +12,9 @@
 # https://stackoverflow.com/questions/2186525/how-to-use-to-find-files-recursively
 
 import os
-import fnmatch
 
 # Add app to the path to prevent errors when commonresources tries to import python_settings
+# Taken from Cameron on StackOverflow: https://stackoverflow.com/a/4383597/25598210
 import sys
 # caution: path[0] is reserved for script path (or '' in REPL)
 sys.path.insert(1, 'app')
@@ -26,10 +26,43 @@ from app.commonresources import info_i, red_x, green_check
 print(info_i()+" linecounter.py by Drew Wingfield")
 print(info_i()+" This is intended to be a temporary thing and should be deleted before release.")
 
+
+
+def pretty_percent(lines_to_count,total_lines):
+    """ Make a percentage from a division, round it to two places """
+    return round((lines_to_count/total_lines)*100,2)
+
 path_to_directory = str(os.path.dirname(os.path.realpath(__file__)))
+
+# Get .gitignore files
+with open(".gitignore","r") as ignore_file:
+    ignore_list = [line for line in ignore_file]
+
+ignore_list = [line.replace("\n","") for line in ignore_list if line!="\n"]
+ignore_list = [line[:-1] if line[-1]=="/" else line for line in ignore_list]
+
+# print("Ignore list:")
+# for i in ignore_list:
+#     print("  - "+str(i))
+
+ignore_list.append(".git")
 
 matches = []
 for root, dirnames, filenames in os.walk(path_to_directory):
+    # Excluding directories/files from os.walk with help from unutbu on StackOverflow:
+    # https://stackoverflow.com/a/19859907/25598210
+    # print("BEFORE")
+    # print(f"root: {root}")
+    # print(f"dirnames: {dirnames}")
+    # print(f"filenames: {filenames}")
+    
+    dirnames[:] = [d for d in dirnames if d not in ignore_list]
+    filenames[:] = [f for f in filenames if f not in ignore_list]
+    # print("AFTER")
+    # print(f"root: {root}")
+    # print(f"dirnames: {dirnames}")
+    # print(f"filenames: {filenames}")
+
     for filename in filenames:
         if filename.endswith((".sh",".ps1",".py")):
             matches.append(os.path.join(root, filename))
@@ -45,8 +78,9 @@ total_line_count = 0
 line_count_uncommented = 0
 
 lines_with_print_statements = 0
-lines_with_if_statements = 0
+lines_with_if_statements  = 0
 lines_with_for_statements = 0
+lines_with_indents = 0
 functions = 0
 classes   = 0
 todos     = 0
@@ -60,6 +94,13 @@ for file_path in matches:
         for line in current_file:
             try:
                 line = line.rstrip()
+
+                if line.startswith("    "):
+                    lines_with_indents += 1
+                    # Remove the indents so startswith detection works.
+                    while "    "==line[:3]:
+                        line = line[4:]
+                
                 if line.startswith("#"):
                     total_line_count +=1
                 
@@ -81,7 +122,7 @@ for file_path in matches:
                         todos+=1
             
             except UnicodeDecodeError as e:
-                print(line)
+                print(f"\n{red_x()} UnicodeDecode Error! line: '{line}'")
                 raise e
 
 
@@ -91,13 +132,15 @@ for file_path in matches:
 
 print()
 print(green_check()+" Done!")
-print(info_i()+f" Read {total_file_count} files.")
+print(green_check()+f" Read {total_file_count} files.")
 print(info_i()+f" Total lines: {total_line_count}")
-print(info_i()+f" Non-comment lines: {line_count_uncommented} - {round(line_count_uncommented/total_line_count,4)*100}% of all lines")
-print(info_i()+f" lines with print: {lines_with_print_statements} - {round((lines_with_print_statements/line_count_uncommented)*100,2)}% of code")
+print(info_i()+f" Non-comment lines: {line_count_uncommented} - {pretty_percent(line_count_uncommented,total_line_count)}% of all lines")
+print(info_i()+f" lines with print: {lines_with_print_statements} - {pretty_percent(lines_with_print_statements,line_count_uncommented)}% of code")
+print(info_i()+f" lines with 1 or more indents: {lines_with_indents} - {pretty_percent(lines_with_indents,line_count_uncommented)}% of code")
 print(info_i()+f" for loops: {lines_with_for_statements}")
 print(info_i()+f" functions: {functions}")
 print(info_i()+f" classes: {classes}")
 print(info_i()+f" todos: {todos}")
+print(green_check()+" Program complete.")
 
 # -- END OF FILE --
