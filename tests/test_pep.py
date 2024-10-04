13f9b6f5fcddd30c11583171c85d4cab391e6bb0
Drew Wingfield <89153952+DrewWing@users.noreply.github.com>
2024-10-04 13:39:48 -0500

    Add trailing commas detection, remove_leading_indents function

--- tests/test_pep.py
+++ tests/test_pep.py
@@ -4,6 +4,12 @@
 
 import unittest
 
+def remove_leading_indents(line):
+    while "    "==line[:3]:
+        line = line[4:]
+
+    return line
+
 class pepTset(unittest.TestCase):
 
     def setUp(self):
@@ -19,9 +25,7 @@ class pepTset(unittest.TestCase):
                 line = line.rstrip()
 
                 # Remove the indents so startswith detection works.
-                if line.startswith("    "):
-                    while "    "==line[:3]:
-                        line = line[4:]
+                line = remove_leading_indents(line)
                 
                 # Remove all trailing comments
                 if "#" in line:
@@ -35,8 +39,36 @@ class pepTset(unittest.TestCase):
                     except AssertionError as e:
                         raise AssertionError(f"PEP 8 nonconformity (importing more than one module in one line) at line {current_line} in file {file_path} \n Line: {line}")
 
-    def test_import_format(self):
+    
+    def single_file_trailing_commas_test(self, file_path):
+        with open(file_path, "r", encoding="utf8") as current_file:
+            # Iterate over every line
+            current_line = 0
+
+            for line in current_file:
+                current_line += 1
+                line = line.rstrip()
+                
+                # Remove all trailing comments
+                if "#" in line:
+                    line = line[:line.index("#")]
+                
+                # If an import is in the line and not a comment
+                try:
+                    assert not(",)" in line or ",]" in line or ",}" in line) or ("test_pep" in file_path) # See the below error message for details.
+                    
+                except AssertionError as e:
+                    raise AssertionError(f"PEP 8 nonconformity (no whitespace between traling comma and close parentheses) at line {current_line} in file {file_path} \n Line: {line}")
+    
+    def test_python_files(self, test_function=None):
+        """ Iterates through all Python files in project and tests using a test function. """
         import os
+        
+        # If the test function is none, return 0.
+        # Because the unittest module auto-runs functions staring with "test_"
+        # and we call this function later to test all files.
+        if test_function==None:
+            return None
 
         with open(".\\.gitignore","r") as ignore_file:
             ignore_list = [line for line in ignore_file]
@@ -61,10 +93,17 @@ class pepTset(unittest.TestCase):
                 if file_name.endswith(".py") and not(root in ignore_list):
                     file_name = os.path.join(root,file_name)
                     #print("    ",file_name) #DEBUG
-                    self.single_file_import_format_test(file_name)
+                    test_function(file_name)
             
         #print("All files:") #DEBUG
-        #rint(all_files) #DEBUG
+        #print(all_files) #DEBUG
+
+    def test_import_formats(self):
+        self.test_python_files(test_function=self.single_file_import_format_test)
+    
+
+    def test_trailing_commas(self):
+        self.test_python_files(test_function=self.single_file_trailing_commas_test)
 
 if __name__ == '__main__':
     unittest.main()
