2babbceed24b52db140b0e30941586a95ecc4d8f
Drew Wingfield <89153952+DrewWing@users.noreply.github.com>
2025-02-07 12:42:05 -0600

    Fix FTC API auth bug, detect invalid response types.

--- app/__main__.py
+++ app/__main__.py
@@ -65,7 +65,7 @@ DELAY_SECONDS   = int(os.getenv("DELAY_SECONDS", 120)) # Seconds between each cy
 ONE_CYCLE_ONLY  = os.getenv("ONE_CYCLE_ONLY", "False").lower() == "true" # Bool, if true only does one cycle
 DRY_RUN         = os.getenv("DRY_RUN","False").lower() == "true"
 DISABLE_API_CALLS       = os.getenv("DISABLE_API_CALLS","False").lower() == "true"
-AUTHORIZATION_HEADER    = "Basic "+os.getenv("PERSONAL_ACCESS_TOKEN", "<placeholder personal access token>")
+AUTHORIZATION_HEADER    = {"authorization":"Basic "+os.getenv("PERSONAL_ACCESS_TOKEN", "<placeholder personal access token>")}
 
 last_update = 0
 last_update_display = "0000-00-00 00:00:00"
@@ -106,7 +106,29 @@ def count_down():
 
 def save_response(response:requests.Response, path):
     """ Saves a response json to a json file. path is relative. """
-    data = response.json()
+
+    # Detect bad status code
+    if response.status_code != requests.status_codes.codes.ok:
+        log_error(f"[__main__] save_response got a status code of {response.status_code} while attempting to save to {path}")
+        log_error(f"[__main__] save_response Response headers (should contain error info): {response.headers}")
+        print(red_x()+f" ERROR: Got an invalid response code ({response.status_code}) while making an API request.")
+        raise Exception(f"[__main__] save_response got a status code of {response.status_code}")
+    
+    # Detect bad response type
+    if "Content-Type" not in response.headers or response.headers["Content-Type"].lower() != "application/json; charset=utf-8":
+        log_error(f"[__main__] save_response - response type is not 'application/json; charset=utf-8' - headers: {response.headers}", level="WARNING")
+
+    try:
+        data = response.json()
+    
+    except requests.exceptions.JSONDecodeError as e:
+        log_error(f"[__main__] save_response was unable to decode the json of a response (response status code {response.status_code}) (saving to path {path}).")
+        log_error(f"Headers of response: {response.headers}")
+        raise e
+    
+    except Exception as e:
+        log_error(f"[__main__] save_response was unable to get the json of a response (saving to path {path})")
+        raise e
     
     with open(os.path.join(PATH_TO_FTCAPI,path),"w") as writer:
         json.dump(data, writer, indent=4)
@@ -187,13 +209,13 @@ def cycle():
     time.sleep(0.1)
 
     if not DISABLE_API_CALLS:
-        print(info_i()+"Getting FTC Event data... (1/3) ")
+        print(info_i()+"Getting FTC Event data 1/3 - Matches")
         if not DRY_RUN: get_matches()
         
-        print("Getting FTC event data 2/3 ")
+        print(info_i()+"Getting FTC event data 2/3 - Schedule")
         if not DRY_RUN: get_schedule()
         
-        print("Getting FTC event data 3/3 ")
+        print(info_i()+"Getting FTC event data 3/3 - Rankings")
         if not DRY_RUN: get_rankings()
 
     else:
@@ -288,8 +310,8 @@ else:
             break
 
 
-print(green_check()+"[__main__] Program has complete")
-#endregion procedura
+print(green_check()+"[__main__] Program has completed")
+#endregion procedural
 
 
 
