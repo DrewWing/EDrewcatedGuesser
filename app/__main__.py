cd4f931a687bdf237f6ba934a6e8a38ddd048c2e
Drew Wingfield <89153952+DrewWing@users.noreply.github.com>
2025-02-21 22:30:16 -0600

    Reconfigure logger, add debug log flush option, add logging to main and OPR.

--- app/__main__.py
+++ app/__main__.py
@@ -54,11 +54,16 @@ import requests
 from dotenv import load_dotenv
 
 # Local Imports
-from common_resources import PATH_TO_FTCAPI, EVENT_CODE, SEASON_YEAR, log_error, green_check, red_x, info_i
-from common_resources import DEBUG_LEVEL, __version__
+from common_resources import PATH_TO_FTCAPI, EVENT_CODE, SEASON_YEAR, create_logger
+from common_resources import __version__
 
 
 #region setup
+FLUSH_DEBUG_LOG = os.getenv("FLUSH_DEBUG_LOG","true").lower() == "true"
+
+logger = create_logger("__main__",flush_debug_log=FLUSH_DEBUG_LOG)
+logger.info("Setting up...")
+
 load_dotenv() # Load environment variables
 
 DELAY_SECONDS   = int(os.getenv("DELAY_SECONDS", 120)) # Seconds between each cycle
@@ -71,10 +76,10 @@ last_update = 0
 last_update_display = "0000-00-00 00:00:00"
 current_iteration = 1
 
-if DEBUG_LEVEL>0:
-    print(info_i()+"EDrewcated Guesser by Drew Wingfield")
-    print(info_i()+"(c) 2025, Drew Wingfield")
-    print(green_check()+"User parameters collected. ")
+logger.info("EDrewcated Guesser by Drew Wingfield")
+logger.info("(c) 2025, Drew Wingfield")
+logger.info(f"Running version {__version__}")
+logger.info("User parameters collected. ")
 
 
 #endregion setup
@@ -82,25 +87,25 @@ if DEBUG_LEVEL>0:
 #region functions
 
 def print_status(message:str):
-    print(info_i()+"")
-    print(info_i()+ " @--------------------------------------@")
-    print(info_i()+ " @ EDrewcated Guesser by Drew Wingfield @")
-    print(info_i()+f" @ v{__version__:16}                    @")
-    print(info_i()+ " @--------------------------------------@")
-    print(info_i()+f" @ Iteration: {current_iteration:20}      @")
-    print(info_i()+f" @ Last Update:    {last_update_display:20} @")
-    print(info_i()+f" @ Current Time:   {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'):20} @")
-    #print(info_i()+f" @ Status:  $status                   ")
-    print(info_i()+f" @ EventCode:      {EVENT_CODE:20} @")
-    print(info_i()+ " @--------------------------------------@")
-    print(info_i()+f" @ {message} ")
-    print(info_i()+ " @--------------------------------------@")
+    logger.info("")
+    logger.info( " @--------------------------------------@")
+    logger.info( " @ EDrewcated Guesser by Drew Wingfield @")
+    logger.info(f" @ v{__version__:16}                    @")
+    logger.info( " @--------------------------------------@")
+    logger.info(f" @ Iteration: {current_iteration:20}      @")
+    logger.info(f" @ Last Update:    {last_update_display:20} @")
+    logger.info(f" @ Current Time:   {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'):20} @")
+    #logger.info(f" @ Status:  $status                   ")
+    logger.info(f" @ EventCode:      {EVENT_CODE:20} @")
+    logger.info( " @--------------------------------------@")
+    logger.info(f" @ {message} ")
+    logger.info( " @--------------------------------------@")
 
 
 
 def count_down():
     for i in range(DELAY_SECONDS, 0, -1):
-        print(info_i()+f"Counting down... {i}")
+        logger.info(f"Counting down... {i}")
         time.sleep(1)
 
 
@@ -109,25 +114,25 @@ def save_response(response:requests.Response, path):
 
     # Detect bad status code
     if response.status_code != requests.status_codes.codes.ok:
-        log_error(f"[__main__] save_response got a status code of {response.status_code} while attempting to save to {path}")
-        log_error(f"[__main__] save_response Response headers (should contain error info): {response.headers}")
-        print(red_x()+f" ERROR: Got an invalid response code ({response.status_code}) while making an API request.")
-        raise Exception(f"[__main__] save_response got a status code of {response.status_code}")
+        logger.error(f"Got an invalid response code ({response.status_code}) while making an API request.")
+        logger.error(f"save_response got a status code of {response.status_code} while attempting to save to {path}")
+        logger.error(f"save_response Response headers (should contain error info): {response.headers}")
+        raise Exception(f"save_response got a status code of {response.status_code}")
     
     # Detect bad response type
     if "Content-Type" not in response.headers or response.headers["Content-Type"].lower() != "application/json; charset=utf-8":
-        log_error(f"[__main__] save_response - response type is not 'application/json; charset=utf-8' - headers: {response.headers}", level="WARNING")
+        logger.warning(f"save_response - response type is not 'application/json; charset=utf-8' - headers: {response.headers}")
 
     try:
         data = response.json()
     
     except requests.exceptions.JSONDecodeError as e:
-        log_error(f"[__main__] save_response was unable to decode the json of a response (response status code {response.status_code}) (saving to path {path}).")
-        log_error(f"Headers of response: {response.headers}")
+        logger.error(f"save_response was unable to decode the json of a response (response status code {response.status_code}) (saving to path {path}).")
+        logger.error(f"Headers of response: {response.headers}")
         raise e
     
     except Exception as e:
-        log_error(f"[__main__] save_response was unable to get the json of a response (saving to path {path})")
+        logger.error(f"save_response was unable to get the json of a response (saving to path {path})")
         raise e
     
     with open(os.path.join(PATH_TO_FTCAPI,path),"w") as writer:
@@ -209,41 +214,41 @@ def cycle():
     time.sleep(0.1)
 
     if not DISABLE_API_CALLS:
-        print(info_i()+"Getting FTC Event data 1/3 - Matches")
+        logger.info("Getting FTC Event data 1/3 - Matches")
         if not DRY_RUN: get_matches()
         
-        print(info_i()+"Getting FTC event data 2/3 - Schedule")
+        logger.info("Getting FTC event data 2/3 - Schedule")
         if not DRY_RUN: get_schedule()
         
-        print(info_i()+"Getting FTC event data 3/3 - Rankings")
+        logger.info("Getting FTC event data 3/3 - Rankings")
         if not DRY_RUN: get_rankings()
 
     else:
-        print(info_i()+"DISABLE_API_CALLS is True. Skipped getting FIRST API data.")
+        logger.info("DISABLE_API_CALLS is True. Skipped getting FIRST API data.")
     
     time.sleep(0.2)
 
-    print(info_i()+"Calculating OPRs... ")
+    logger.info("Calculating OPRs... ")
     
     if DRY_RUN:
-        print(info_i()+"  (Disabled because DRY_RUN is True)")
+        logger.info("  (Disabled because DRY_RUN is True)")
 
     else:
-        print(info_i()+"[__main__] Setting up OPR calculation...")
+        logger.info("Setting up OPR calculation...")
         import OPR as opr_module
         
-        print(info_i()+"[__main__] Performing OPR calculation...")
+        logger.info("Performing OPR calculation...")
         opr_module.master_function()        
-        print(green_check()+"[__main__] OPR calculations commplete.")
-        print(info_i()+"Deleting unused funtions")
+        logger.info("OPR calculations commplete.")
+        logger.info("Deleting unused funtions")
         del opr_module
-        print(green_check()+"Unused functions deleted.")
+        logger.info("Unused functions deleted.")
 
     if not DISABLE_API_CALLS:
-        print(info_i()+"Pushing team data... ")
+        logger.info("Pushing team data... ")
 
         if DRY_RUN:
-            print(info_i()+"Disabled because DRY_RUN is True")
+            logger.info("Disabled because DRY_RUN is True")
         
         else:
             #TODO Run sheets_api stuff here (teams argument)
@@ -299,6 +304,7 @@ def cycle():
 # }
 
 
+logger.info("Setup complete.")
 
 if ONE_CYCLE_ONLY:
     cycle()
@@ -311,11 +317,11 @@ else:
             count_down()
 
         except KeyboardInterrupt:
-            print(red_x()+"Breaking cycle due to keyboard interrupt.")
+            logger.warning("Breaking cycle due to keyboard interrupt.")
             break
 
 
-print(green_check()+"[__main__] Program has completed")
+logger.info("[__main__] Program has completed")
 #endregion procedural
 
 
